2d75a6d2f1d9770e7ace7c6979dff7f153625ea0
Zborovskiy Kyryl <zborovskiy.kyryl@gmail.com>
2021-11-20 22:51:59 +0200

    add first class

---
+++ src/test/java/zborovskiy/kyryl/gitexample/SimpleTest.java
@@ -0,0 +1,14 @@
+package zborovskiy.kyryl.gitexample;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(true);
+    }
+
+
+}
